55a5965500d0de6562f272d0aa15e309e4cce9cc
Sergei12123 <rfrhgrg@yandex.ru>
2024-05-03 12:54:02 +0500

    task3

--- task02/jsrc/main/java/com/cmtrc48239a0helloworldtest/Demojavalambda.java
+++ task02/jsrc/main/java/com/cmtrc48239a0helloworldtest/Demojavalambda.java
@@ -4,7 +4,10 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +17,10 @@ import java.util.Map;
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaUrlConfig(
+	authType = AuthType.NONE,
+	invokeMode = InvokeMode.BUFFERED
 )
 public class Demojavalambda implements RequestHandler<APIGatewayProxyRequestEvent, Map<String, Object>> {
 
